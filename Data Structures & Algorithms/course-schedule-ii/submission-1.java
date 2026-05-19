4cef21b84e1028d70c75a9c0a39068b54dad7ee1
Veena Kumari <30977545+VKSGit2017@users.noreply.github.com>
2026-05-19 11:51:59 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.java
@@ -0,0 +1,30 @@
+//Time Complexity - O(V+E)
+//Space Complexity - O(V+E)
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    List<List<Integer>> adj = new ArrayList<>();
+    int[] inDegree = new int[numCourses];
+
+    for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+    for (int[] pre : prerequisites) {
+        adj.get(pre[1]).add(pre[0]);
+        inDegree[pre[0]]++;
+    }
+
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 0; i < numCourses; i++) {
+        if (inDegree[i] == 0) queue.offer(i);
+    }
+
+    int[] order = new int[numCourses];
+    int idx = 0;
+    while (!queue.isEmpty()) {
+        int course = queue.poll();
+        order[idx++] = course;
+        for (int next : adj.get(course)) {
+            if (--inDegree[next] == 0) queue.offer(next);
+        }
+    }
+    return idx == numCourses ? order : new int[0]; // Empty = cycle detected
+}
+}
